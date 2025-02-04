08d4b1e9972a3dc6e514cd10e0cddc6b25c212f2
mckeowr1 <68450067+mckeowr1@users.noreply.github.com>
2025-02-04 10:07:09 -0600

    add HTA res thresholds

--- scripts/hta_res_threshold/hta_res_threshold.R
+++ scripts/hta_res_threshold/hta_res_threshold.R
@@ -3,60 +3,13 @@ library(data.table)
 
 set.seed(123)
 
-#### Functions ####
-
-# Function also used in `normalize_phenotypes.R`
-# to flag strains as resistant or sensitive
-#' Get Resistant Strains
-#'
-#' This function calculates the resistance threshold and identifies strains with resistance based on a specified phenotype column.
-#'
-#' @param exp_summary_df A data frame containing the experimental summary with columns `strain` and a dynamic phenotype column.
-#' @param pheno_col A string specifying the name of the phenotype column to be used for resistance calculation.
-#' @param threshold_per A numeric value representing the threshold percentage to classify strains as resistant. Default is 0.50.
-#' @param ref_strain Reference strain to calculate the resistance threshold.
-#'
-#' @return A list containing:
-#' \item{min}{The minimum value of the specified phenotype column.}
-#' \item{max}{The maximum value of the specified phenotype column.}
-#' \item{res_strains}{A vector of strains identified as resistant based on the threshold.}
-#' \item{threshold}{The calculated threshold value used for resistance classification.}
-#'
-# Set a resistance threshold using the normalized phenotypes
-# Function to calculate the resistance threshold and identify strains with resistance
-# function expects a data frame with columns `strain` and a dynamic `pheno_col`
-get_res_strains_ref <- function(exp_summary_df, pheno_col, ref_strain, threshold_per = 0.50) {
-  # exp_summary_df: a data frame with the columns `strain` and a dynamic `pheno_col`
-  # max <- exp_summary_df %>%
-  #   dplyr::pull(!!sym(pheno_col)) %>%
-  #   max()
-
-  min <- exp_summary_df %>%
-    dplyr::filter(strain == ref_strain) %>%
-    dplyr::pull(!!sym(pheno_col)) %>%
-    min()
-
-  res_thres <- (min * -threshold_per) + min  
-
-  res_strains <- exp_summary_df %>%
-    dplyr::filter(!!sym(pheno_col) > res_thres) %>%
-    dplyr::pull(strain)
-
-  out <- list(
-    min = min,
-    max = max,
-    res_strains = res_strains,
-    threshold = res_thres
-  )
-
-  return(out)
-}
+source("bin/get_res_threshold.R")
 
 #### Output ####
 
 # output directory named with the summary
 data_out_dir <- glue::glue(
-  "data/proc/hta_res_threshold"
+  "data/tables/hta_res_threshold"
 )
 
 # if it does exist, delete the contents
@@ -71,13 +24,13 @@ dir.create(data_out_dir, recursive = TRUE)
 ref_threshold <- 0.75
 
 ce_hta_file <-
-    "data/raw/species_hta/elegans_df_ABZ_summary_mean_median_wormlength.csv"
+    "data/hta_summaries/elegans_df_ABZ_summary_mean_median_wormlength.csv"
 
 cb_hta_file <-
-    "data/raw/species_hta/briggsae_df_ABZ_summary_mean_median_wormlength.csv"
+    "data/hta_summaries/briggsae_df_ABZ_summary_mean_median_wormlength.csv"
 
 ct_hta_file <-
-    "data/raw/species_hta/tropicalis_df_ABZ_summary_mean_median_wormlength.csv"
+    "data/hta_summaries//tropicalis_df_ABZ_summary_mean_median_wormlength.csv"
 
 ce_hta <- fread(ce_hta_file)
 cb_hta <- fread(cb_hta_file)
